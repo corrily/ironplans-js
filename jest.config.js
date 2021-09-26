8496efc0aef6bcf3666b5f6828fdfd3571b7d112
Joseph Gilley <j@banditml.com>
2021-09-26 12:34:50 -0700

    chore: setup monorepo testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+}
