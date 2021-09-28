cb3b1b9f4bfc5537819f6b07af9222f9bf0cdcc0
Joseph Gilley <j@banditml.com>
2021-09-28 00:59:49 -0700

    feat(proxy): add lifecycle methods and dynamic port

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,8 +1,10 @@
 import { ProviderToJSON } from '@ironplans/api'
 import * as IPTypes from '@ironplans/types'
+import getPort from 'get-port'
 import jwt from 'jsonwebtoken'
 import { JwtPayload } from 'jwt-decode'
 import Koa from 'koa'
+import { Server as NodeServer } from 'http'
 import { paginate, Routes, StubResponse, withDates } from './stub'
 
 const providerClaim = 'https://api.ironplans.com/.jwt/provider/'
@@ -34,6 +36,8 @@ export class Server extends Koa {
   providerId: string
 
   customerId: string
+
+  server?: NodeServer
 
   teams: IPTypes.components['schemas']['Team'][]
 
@@ -98,14 +102,18 @@ export class Server extends Koa {
     })
 
     this.use((ctx) => {
-      const route = this.findRoute(ctx.method, ctx.path)
-      if (!route) {
-        ctx.throw(500, `${ctx.method} ${ctx.url} not handled`)
-        return
+      try {
+        const route = this.findRoute(ctx.method, ctx.path)
+        if (!route) {
+          ctx.throw(500, `${ctx.method} ${ctx.url} not handled`)
+          return
+        }
+        const { status, data } = route({ server: this, request: ctx.request })
+        ctx.status = status ?? 200
+        ctx.body = data
+      } catch (e) {
+        ctx.throw(500, `${e}`)
       }
-      const { status, data } = route({ server: this, request: ctx.request })
-      ctx.status = status ?? 200
-      ctx.body = data
     })
   }
 
@@ -115,5 +123,17 @@ export class Server extends Koa {
         matcher.test(path) && m.toLowerCase() === method.toLowerCase()
     )
     return route?.handler
+  }
+
+  async start(port?: number): Promise<number> {
+    return new Promise((resolve) => {
+      getPort({ port }).then((p) => {
+        this.server = this.listen(p, () => resolve(p))
+      })
+    })
+  }
+
+  async stop() {
+    this.server?.close()
   }
 }
